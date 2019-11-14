2164ba89086440621285fce35442330245abec90
Iskander Iskakov <tradeshells@gmail.com>
2019-11-13 20:13:45 -0800

    evolution class added

--- dagestan.py
+++ dagestan.py
@@ -9,59 +9,78 @@ fittest_actions = np.empty([420, 4])
 population = []
 
 
+
 class Marvin:
     
-    def __init__(self):
-        self.actions = np.empty([420, 4])
+    def __init__(self, population_size, genome_size, mut_chance=0.1):
+        self.actions = np.empty([genome_size, 4])
         self.fitness = 0
         self.done = False
+        self.population_size = population_size
+        self.mut_chance = mut_chance
+        self.fill_random_actions()
 
 
-    def random_actions(self):
-        for i in range(420):
+    def fill_random_actions(self):
+        for i in range(self.population_size):
             self.actions[i] = env.action_space.sample()
 
     def walk(self):
-        observation_n = env.reset()
-        for action in self.actions:
-            if self.done == False:
-                observation_n, reward_n, done_n, info = env.step(action)
-                env.render()
-                self.fitness += reward_n
-                self.done = done_n
-                if self.fitness < -10:
-                    self.done = True
+        while(True and self.done == False):
+            for action in self.actions:
+                if self.done == False:
+                    observation_n, reward_n, done_n, info = env.step(action)
+                    env.render()
+                    self.fitness += reward_n
+                    self.done = done_n
+                    if self.fitness < -10:
+                        self.done = True
         self.fitness = (self.fitness - -120) / (300 - -120)
-        print(self.fitness)
+
 
 class Population:
 
-    def __init__(self, population=False):
+    def __init__(self, size, genome_size, population=False):
         if population:
             self.population = population
         else:
             self.population = []
-        fittest = 0
+        self.fittest = 0
+        self.genome_size = genome_size
+        self.size = size
+        print("New population created", self.size, self.genome_size)
 
     def create(self):
-        for i in range(100):
-            population.append(Marvin())
+        for i in range(self.size):
+            population.append(Marvin(self.size, self.genome_size))
 
     def compete(self):
-        for i in range(100):
-            (population[i]).random_actions()
+        for i in range(self.size):
+            env.reset()
             (population[i]).walk()
-        print(population[i].fitness)
+            print(population[i].fitness)
         if (population[i].fitness > fittest):
             self.fittest = population[i].fitness
             fittest_actions = population[i].actions
         print("I was the fittest, my fitness is ", self.fittest)
 
-        
-first = Population()
+class Evolution:
 
-first.create()
-first.compete()
+    def __init__(self, p_size, g_size, mut_chance=0.1):
+        self.population_size = p_size
+        #self.current_population = Population
+        self.genome_size = g_size
+        self.mutation_chance = mut_chance
+
+    def soup(self):
+        self.current_population = Population(self.population_size, self.genome_size) 
+        self.current_population.create()
+        self.current_population.compete()
+
+
+life = Evolution(100, 400)        
+life.soup()
+
 
 
 
